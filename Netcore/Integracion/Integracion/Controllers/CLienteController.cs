3229a464164053ce96c865d689aa9a5e0c8aeb86
Mrempanada1 <101122089+Mrempanada1@users.noreply.github.com>
2023-05-14 15:39:11 -0400

    Integracion
    Rest api C# ASP.NET Core ( servicios )

---
+++ Netcore/Integracion/Integracion/Controllers/CLienteController.cs
@@ -0,0 +1,70 @@
+﻿using Integracion.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Integracion.Controllers
+        
+{
+    [ApiController]
+    [Route("[cliente]")]
+    public class CLienteController: ControllerBase
+    {
+        [HttpGet]
+        [Route("listar")]
+        public dynamic listarCliente()
+        {
+            // Listar clientes
+
+            List<Cliente> clientes = new List<Cliente>
+            {
+                new Cliente
+                {
+                    id = "1",
+                    nombre = "Tama",
+                    descripcion = "bateria tama",
+                    categoria = "Percusion",
+                    precio = "250000",
+                    stock = "12"
+                }
+            };
+            return clientes;
+        }
+        [HttpPatch]
+        [Route("modificar")]
+        public dynamic modificarCliente()
+        {
+            return new
+            {
+
+            };
+        }
+        [HttpPost]
+        [Route("guardar")]
+        public dynamic guardarCliente()
+        {
+            return new
+            {
+
+            };
+
+        }
+        [HttpPut]
+        [Route("actualizar")]
+        public dynamic actualizarCliente()
+        {
+            return new
+            {
+
+            };
+        }
+        [HttpDelete]
+        [Route("Eliminar")]
+        public dynamic eliminarCliente()
+        {
+            return new
+            {
+
+            };
+        }
+
+    }
+}
---
+++ Netcore/Integracion/Integracion/Models/Cliente.cs
@@ -0,0 +1,12 @@
+﻿namespace Integracion.Models
+{
+    public class Cliente
+    {
+        public string id { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public string categoria { get; set; }
+        public string precio { get; set; }
+        public string stock { get; set; }
+    }
+}
